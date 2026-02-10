d0ea1ae2241c2ef29c75ec10f0b9da6388e244dc
ryntnmsn <127068317+ryntnmsn@users.noreply.github.com>
2026-02-10 09:03:07 +0800

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,10 @@
 export default function Header() {
   return (
     <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
-      <img src="/assets/profile_img.png" alt="" className="rounded-full w-32" />
+      <img src="/portfolio/assets/profile_img.png" alt="" className="rounded-full w-32" />
       <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
         Hi! I&apos;m Ryan
-        <img src="/assets/hand-icon.png" alt="" className="w-6 mb-1" />
+        <img src="/portfolio/assets/hand-icon.png" alt="" className="w-6 mb-1" />
       </h3>
       <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo">
         web developer and graphic artist
@@ -20,17 +20,17 @@ export default function Header() {
           className="px-10 py-2.5 border rounded-full bg-slate-800 text-white flex items-center gap-2 dark:border-transparent"
         >
           contact me{" "}
-          <img src="/assets/right-arrow-white.png" alt="" className="w-4" />
+          <img src="/portfolio/assets/right-arrow-white.png" alt="" className="w-4" />
         </a>
 
         <a
-          href="/assets/Ryan_Tinamisan_Resume.pdf"
+          href="/portfolio/assets/Ryan_Tinamisan_Resume.pdf"
           download="Ryan_Tinamisan_Resume.pdf"
           className="px-10 py-2.5 rounded-full border border-gray-300 dark:border-white/25 hover:bg-slate-100/70 dark:hover:bg-darkHover flex items-center gap-2 bg-white dark:bg-transparent dark:text-white"
         >
           my resume{" "}
           <img
-            src="/assets/download-icon.png"
+            src="/portfolio/assets/download-icon.png"
             alt=""
             className="w-4 dark:invert"
           />
